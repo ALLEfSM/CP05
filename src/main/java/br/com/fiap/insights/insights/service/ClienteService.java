f8a4d0108a45084083899fbb82e3f376a7ca95a5
ALLEF SANTOS <125573405+ALLEfSM@users.noreply.github.com>
2024-10-10 21:45:30 -0300

    Add files via upload

---
+++ src/main/java/br/com/fiap/insights/insights/service/ClienteService.java
@@ -0,0 +1,14 @@
+//package br.com.fiap.insights.insights.service;
+//
+//import br.com.fiap.insights.insights.model.Cliente;
+//
+//import java.util.List;
+//
+//public interface ClienteService {
+//
+//    public Cliente findClienteByNome(String nome);
+//
+//    public List<Cliente> findAllClientes(String nome);
+//
+//
+//}
